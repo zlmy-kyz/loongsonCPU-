cac90101369ed45892beec9d7ec153a91063b573
zlmy-kyz <1511454503@qq.com>
2026-07-26 14:26:43 +0800

    andi,ori,xori编写

--- exp10/myCPU/mycpu_top.v
+++ exp10/myCPU/mycpu_top.v
@@ -101,6 +101,9 @@ wire        inst_lu12i_w;
 wire        inst_pcaddu12i;
 wire        inst_slti;
 wire        inst_sltui;
+wire        inst_andi;
+wire        inst_ori;
+wire        inst_xori;
 
 wire        need_ui5;
 wire        need_si12;
@@ -108,6 +111,7 @@ wire        need_si16;
 wire        need_si20;
 wire        need_si26;
 wire        src2_is_4;
+wire        need_ui12;
 
 wire [ 4:0] rf_raddr1;
 wire [31:0] rf_rdata1;
@@ -230,16 +234,19 @@ assign inst_lu12i_w = op_31_26_d[6'h05] & ~inst[25];
 assign inst_pcaddu12i = op_31_26_d[6'h07] & ~inst[25];
 assign inst_slti   = op_31_26_d[6'h00] & op_25_22_d[4'h8];
 assign inst_sltui  = op_31_26_d[6'h00] & op_25_22_d[4'h9];
+assign inst_andi   = op_31_26_d[6'h00] & op_25_22_d[4'hd];
+assign inst_ori    = op_31_26_d[6'h00] & op_25_22_d[4'he];
+assign inst_xori   = op_31_26_d[6'h00] & op_25_22_d[4'hf];
 
 assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
                     | inst_jirl | inst_bl | inst_pcaddu12i;
 assign alu_op[ 1] = inst_sub_w;
 assign alu_op[ 2] = inst_slt | inst_slti;
 assign alu_op[ 3] = inst_sltu | inst_sltui;
-assign alu_op[ 4] = inst_and;
+assign alu_op[ 4] = inst_and | inst_andi;
 assign alu_op[ 5] = inst_nor;
-assign alu_op[ 6] = inst_or;
-assign alu_op[ 7] = inst_xor;
+assign alu_op[ 6] = inst_or | inst_ori;
+assign alu_op[ 7] = inst_xor | inst_xori;
 assign alu_op[ 8] = inst_slli_w;
 assign alu_op[ 9] = inst_srli_w;
 assign alu_op[10] = inst_srai_w;
@@ -249,11 +256,13 @@ assign need_ui5   =  inst_slli_w | inst_srli_w | inst_srai_w;
 assign need_si12  =  inst_addi_w | inst_ld_w | inst_st_w;
 assign need_si16  =  inst_jirl | inst_beq | inst_bne;
 assign need_si20  =  inst_lu12i_w | inst_pcaddu12i;
+assign need_ui12  =  inst_andi | inst_ori | inst_xori;
 assign need_si26  =  inst_b | inst_bl;
 assign src2_is_4  =  inst_jirl | inst_bl;
 
 assign imm = src2_is_4 ? 32'h4                      :
              need_si20 ? {i20[19:0], 12'b0}         :
+             need_ui12 ? {20'b0, i12[11:0]}         :
 /*need_ui5 || need_si12*/{{20{i12[11]}}, i12[11:0]} ;
 
 assign br_offs = need_si26 ? {{ 4{i26[25]}}, i26[25:0], 2'b0} :
@@ -276,7 +285,10 @@ assign src2_is_imm   = inst_slli_w |
                        inst_bl     |
                        inst_pcaddu12i|
                        inst_slti   |
-                       inst_sltui  ;
+                       inst_sltui  |
+                       inst_andi   |
+                       inst_ori    |
+                       inst_xori   ;
 
 assign res_from_mem  = inst_ld_w;
 assign dst_is_r1     = inst_bl;
